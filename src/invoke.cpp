a4da9237355460a90aa4d88ba5450e137b445d2e
ryan <ryan.chow@utexas.edu>
2020-07-15 09:08:51 -0500

    Adding executable to CMakeLists.txt Changing invoke to not use boost libraries.

--- src/invoke.cpp
+++ src/invoke.cpp
@@ -1,30 +1,32 @@
 #include <sdbusplus/bus.hpp>
 
+#include <cstdint>
 #include <iostream>
+
+/** An example dbus client application.
+ *   *  Calls org.freedesktop.login1's ListUsers interface to find all active
+ *     *  users in the system and displays their username.
+ *       */
 
 int main()
 {
-    boost::asio::io_context io;
-    auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+    using namespace sdbusplus;
 
-    int ready = 0;
-    while (!ready)
+    auto b = bus::new_default_system();
+    auto m =
+        b.new_method_call("org.freedesktop.login1", "/org/freedesktop/login1",
+                "org.freedesktop.login1.Manager", "ListUsers");
+    auto reply = b.call(m);
+
+    std::vector<std::tuple<uint32_t, std::string, message::object_path>> users;
+    reply.read(users);
+
+    for (auto& user : users)
     {
-        auto readyMsg = conn->new_method_call(
-            "xyz.openbmc_project.asio-test", "/xyz/openbmc_project/test",
-            "xyz.openbmc_project.test", "VoidFunctionReturnsInt");
-        try
-        {
-            message intMsg = conn->call(readyMsg);
-            intMsg.read(ready);
-        }
-        catch (sdbusplus::exception::SdBusError& e)
-        {
-            ready = 0;
-            // pause to give the server a chance to start up
-            usleep(10000);
-        }
+        std::cout << std::get<std::string>(user) << "\n";
+
     }
-    std::cout << "ready received\n";
+
     return 0;
+
 }
